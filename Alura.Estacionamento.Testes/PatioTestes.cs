3a291b8821f973ba2d079c50d3405a818ce77ca6
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2022-04-10 13:10:47 -0300

    Automacao de Testes - 02 Utilizando xUnit

---
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,36 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void TesteValidaFaturamento()
+        {
+            //arrange            
+            var veiculo = new Veiculo {
+                Proprietario = "Andre Silva",
+                Tipo = TipoVeiculo.Automovel,
+                Cor = "Verde",
+                Modelo = "Fusca",
+                Placa = "ASD-9999"
+            };
+            var estacionamento = new Patio();
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //assert
+            Assert.Equal(2,faturamento);
+        }
+    }
+}

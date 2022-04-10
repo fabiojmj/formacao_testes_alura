6150f6f34df9357807cacb9a0bcecb480cff0181
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2022-04-10 12:35:46 -0300

    aula 09 - O que aprendemos

---
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,48 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class VeiculosTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            
+            //Act
+            veiculo.Acelerar(10);
+
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Frear(10);
+
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            //arrange
+            var veiculo = new Veiculo();
+            //act
+            //assert
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
+        }
+
+    }
+}
